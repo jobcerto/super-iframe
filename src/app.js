3e94e85f1c70f45ee63e8d1c1ddf688bfc6837cd
Eduardo Geschonke <73890223+cticcapital@users.noreply.github.com>
2024-01-11 11:38:01 -0300

    wip

--- src/app.js
+++ src/app.js
@@ -3,6 +3,11 @@ import queryString from 'query-string';
 
 import Cookies from 'js-cookie';
 import collect from 'collect.js';
+import Alpine from 'alpinejs'
+
+window.Alpine = Alpine
+
+Alpine.start()
 
 const app = () => {
 
@@ -32,11 +37,16 @@ const app = () => {
 const initializeHeader = () => {
     let cassinoTipsHeader = document.createElement('div');
 
-    cassinoTipsHeader.innerHTML = `
+    cassinoTipsHeader.innerHTML = `<header class="tw-antialiased tw-text-base tw-text-green-400 tw-font-sans tw-bg-green-950 tw-shadow-lg tw-border-b-4 tw-border-green-900 tw-h-24 tw-px-4 lg:tw-px-0 tw-py-4 tw-flex tw-items-center tw-sticky tw-top-0 tw-z-50 tw-relative">
+
     <div class="tw-container tw-mx-auto tw-flex tw-items-center tw-justify-between">
+        <a href="#">
+            <img src="https://cassinotips.com.br/images/greenfortuna.png" alt="Cassino Tips" class="tw-h-10 sm:tw-h-12 md:tw-h-16">
+        </a>
+
         <nav class="tw-inline-flex tw-items-center tw-gap-x-4 md:tw-gap-x-8">
             <a href="#">
-                <span class="far fa-bell text-2xl"></span>
+                <span class="far fa-bell tw-text-2xl"></span>
             </a>
             <span class="tw-bg-gray-800 tw-border tw-border-2 tw-border-green-700 tw-text-green-500 tw-rounded-md tw-px-1.5 md:tw-px-3 tw-py-1.5 tw-text-xs md:tw-text-sm">Sincronizado</span>
 
@@ -45,13 +55,60 @@ const initializeHeader = () => {
             </a>
         </nav>
     </div>
-    `;
 
-// Add the CSS class to the new div
-    cassinoTipsHeader.className = 'tw-bg-green-950 tw-shadow-lg tw-border-b-4 tw-border-green-900 tw-h-24 tw-px-4 lg:tw-px-0 tw-py-4 tw-flex tw-items-center tw-sticky tw-top-0 tw-z-50 tw-relative';
+    <nav>
 
-// Find the div with ID 'fe_header'
-    let header = document.getElementById('fe_header');
+</nav>
+</header>
+<section  x-data="{
+            secondsBeforeNextSignal:0,
+            seconds(seconds) {
+               this.secondsBeforeNextSignal = seconds;
+            },
+            countdown() {
+                if (this.secondsBeforeNextSignal > 0) {
+                    this.secondsBeforeNextSignal--;
+                }
+            }
+        }" x-init="setInterval(() => countdown(), 1000)" class="tw-h-full tw-py-4">
+    <nav class="tw-font-sans tw-grid sm:tw-grid-cols-2 lg:tw-grid-cols-4 tw-gap-4 tw-w-full" x-show.transition="secondsBeforeNextSignal !== 0" style="display: none;">
+
+       <div class="tw-bg-green-900 tw-rounded-md tw-px-4 tw-py-6 tw-text-xl tw-font-bold tw-text-green-200">
+            🕑 Válido até: 11:33
+        </div>
+            <div class="tw-bg-green-900 tw-rounded-md tw-px-4 tw-py-6 tw-text-xl tw-font-bold tw-text-green-200">
+            👉 7x Normal
+        </div>
+            <div class="tw-bg-green-900 tw-rounded-md tw-px-4 tw-py-6 tw-text-xl tw-font-bold tw-text-green-200">
+            ⚡ 9x Turbo
+        </div>
+            <div class="tw-bg-green-900 tw-rounded-md tw-px-4 tw-py-6 tw-text-xl tw-font-bold tw-text-green-200">
+            🎲 Nº de Jogadas: 12
+        </div>
+  
+</nav>
+    <div class="tw-w-full tw-flex tw-items-center tw-justify-center tw-mt-4">
+    <button x-show="secondsBeforeNextSignal !== 0" type="button" class="tw-w-full tw-bg-green-950 tw-text-center tw-text-green-500 tw-rounded-md tw-p-3 tw-text-xl tw-font-medium" disabled="" style="display: none;">
+        <i class="fas fa-spin fa-spinner"></i> Aguarde
+        <span x-text="secondsBeforeNextSignal">0</span> para gerar novo sinal
+    </button>
+
+    <button @click="seconds(179)" x-show="secondsBeforeNextSignal === 0" type="button" class="tw-w-full tw-bg-green-700 tw-text-center tw-text-white tw-rounded-md tw-p-3 tw-text-xl tw-font-bold">
+        <i class="far fa-check-circle"></i> GERAR SINAL
+    </button>
+</div>
+
+    
+</section>
+
+
+`;
+
+    let header = document.getElementById('fe_web_container');
+
+    header.className = 'cassinotips';
+
+    cassinoTipsHeader.className = 'tw-bg-green-975';
 
     insertNodeAfter(cassinoTipsHeader, header);
 }
